04553bb99fba2d67967508f686ffcc38e05ebd2d
taigiiz <taivan.amgalan@gmail.com>
2019-11-20 11:08:46 +0800

    RETURN TEST

--- MICA_WEB/MICA_WEB/Controllers/HomeController.cs
+++ MICA_WEB/MICA_WEB/Controllers/HomeController.cs
@@ -12,29 +12,5 @@ namespace MICA_WEB.Controllers
         {
             return View();
         }
-
-        public ActionResult IndexAmgaa1()
-        {
-            return View();
-        }
-        public ActionResult IndexAmgaa2()
-        {
-            return View();
-        }
-
-        public ActionResult IndexSonixmaa()
-        {
-            return View();
-        }
-
-        public ActionResult IndexJagaa()
-        {
-            return View();
-        }
-
-        public ActionResult TaivanChanges()
-        {
-            return View();
-        }
     }
 }
