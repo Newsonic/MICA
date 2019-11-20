855cc0c3dab513e98f29ba00bb443e7f2d920c80
Sonix <Sonix@DESKTOP-71M7BGS>
2019-11-20 10:21:54 +0800

    test

--- MICA_WEB/MICA_WEB/Controllers/HomeController.cs
+++ MICA_WEB/MICA_WEB/Controllers/HomeController.cs
@@ -17,5 +17,10 @@ namespace MICA_WEB.Controllers
         {
             return View();
         }
+
+        public ActionResult IndexSonixmaa()
+        {
+            return View();
+        }
     }
 }
